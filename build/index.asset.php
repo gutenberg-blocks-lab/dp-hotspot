b863a37b5a8383a8804cda174ebd8626418ac470
Marko Krstic <mailme@markokrstic.com>
2024-01-08 18:37:59 +0100

    ....

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7eda8a6d52198a175a77');
--- src/edit.js
+++ src/edit.js
@@ -1,70 +1,96 @@
-// Edit.js
-
 import { __ } from "@wordpress/i18n";
 import { useBlockProps, InspectorControls } from "@wordpress/block-editor";
 import {
-	PanelBody,
-	__experimentalNumberControl as NumberControl,
-	Flex,
-	FlexBlock,
-	FlexItem,
+    PanelBody,
+    __experimentalNumberControl as NumberControl,
+    Flex,
+    Button,
 } from "@wordpress/components";
 import "./editor.scss";
 
 export default function Edit({ attributes, setAttributes }) {
-	const { hotspotNumbers } = attributes;
+    const { hotspotNumbers } = attributes;
 
-	const addHotspotNumber = () => {
-		const newHotspotNumber = { top: 0, left: 0 };
-		setAttributes({ hotspotNumbers: [...hotspotNumbers, newHotspotNumber] });
-	};
+    const addHotspotNumber = () => {
+        const newHotspotNumber = { bottom: 0, left: 0 };
+        setAttributes({
+            hotspotNumbers: [...hotspotNumbers, newHotspotNumber],
+        });
+    };
 
-	const updateHotspotNumber = (index, updatedItem) => {
-		const updatedItems = hotspotNumbers.map((item, i) =>
-			index === i ? { ...item, ...updatedItem } : item,
-		);
-		setAttributes({ hotspotNumbers: updatedItems });
-	};
+    const updateHotspotNumber = (index, key, newValue) => {
+        const newHotspotNumbers = [...hotspotNumbers];
+        newHotspotNumbers[index] = {
+            ...newHotspotNumbers[index],
+            [key]: newValue,
+        };
+        setAttributes({ hotspotNumbers: newHotspotNumbers });
+    };
 
-	const removeHotspotNumber = (index) => {
-		const updatedItems = hotspotNumbers.filter((_, i) => i !== index);
-		setAttributes({ hotspotNumbers: updatedItems });
-	};
+    const removeHotspotNumber = (index) => {
+        const updatedItems = hotspotNumbers.filter((_, i) => i !== index);
+        setAttributes({ hotspotNumbers: updatedItems });
+    };
 
-	return (
-		<>
-			<InspectorControls>
-				<PanelBody title={__("Position Settings", "dp-hotspot")}>
-					{hotspotNumbers.map((item, index) => (
-						
-							<div key={index}>
-								<Flex>
-								<NumberControl
-									label={__("Top", "dp-hotspot")}
-									value={item.top}
-									onChange={(newTop) =>
-										updateHotspotNumber(index, { top: newTop })
-									}
-								/>
-								<NumberControl
-									label={__("Left", "dp-hotspot")}
-									value={item.left}
-									onChange={(newLeft) =>
-										updateHotspotNumber(index, { left: newLeft })
-									}
-								/>
-								<button onClick={() => removeHotspotNumber(index)}>
-									Remove
-								</button>
-								</Flex>
-							</div>
-					))}
-					<button onClick={addHotspotNumber}>Add Hotspot</button>
-				</PanelBody>
-			</InspectorControls>
-			<p {...useBlockProps()}>
-				{__("Dp Hotspot – hello from the editor!", "dp-hotspot")}
-			</p>
-		</>
-	);
+    return (
+        <>
+            <InspectorControls>
+                <PanelBody title={__("Position Settings", "dp-hotspot")}>
+                    {hotspotNumbers.map((item, index) => (
+                        <div key={index}>
+                            <Flex>
+                                <NumberControl
+                                    label={__("Bottom (%)", "dp-hotspot")}
+                                    value={item.bottom}
+                                    onChange={(newBottom) =>
+                                        updateHotspotNumber(
+                                            index,
+                                            "bottom",
+                                            newBottom
+                                        )
+                                    }
+                                />
+                                <NumberControl
+                                    label={__("Left (%)", "dp-hotspot")}
+                                    value={item.left}
+                                    onChange={(newLeft) =>
+                                        updateHotspotNumber(
+                                            index,
+                                            "left",
+                                            newLeft
+                                        )
+                                    }
+                                />
+                                <Button
+                                    variant="tertiary"
+                                    onClick={() => removeHotspotNumber(index)}
+                                >
+                                    Del
+                                </Button>
+                            </Flex>
+                        </div>
+                    ))}
+                    <Button onClick={addHotspotNumber} variant="primary">
+                        Add Hotspot
+                    </Button>
+                </PanelBody>
+            </InspectorControls>
+            <div {...useBlockProps()}>
+                {__("Dp Hotspot – hello from the editor!", "dp-hotspot")}
+                {hotspotNumbers.map((item, index) => (
+                    <div
+                        className="hotspotPoint"
+                        key={index}
+                        style={{
+                            position: "absolute",
+                            bottom: `${item.bottom}%`,
+                            left: `${item.left}%`,
+                        }}
+                    >
+                        {index + 1}
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
