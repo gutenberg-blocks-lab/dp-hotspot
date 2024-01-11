2708b10ad54273ba903ebc056f2ab6abb1dcab53
Marko Krstic <mailme@markokrstic.com>
2024-01-11 11:23:27 +0100

    updated frontend

--- src/save.js
+++ src/save.js
@@ -2,10 +2,25 @@
 
 import { useBlockProps, InnerBlocks } from "@wordpress/block-editor";
 
-export default function save() {
+export default function save({ attributes }) {
+    const { hotspotNumbers } = attributes;
+
     return (
         <div {...useBlockProps.save()}>
             <InnerBlocks.Content />
+            {hotspotNumbers.map((hotspot, index) => (
+                <div
+                    key={index}
+                    className="hotspotPoint"
+                    style={{
+                        position: "absolute",
+                        bottom: `${hotspot.bottom}%`,
+                        left: `${hotspot.left}%`,
+                    }}
+                >
+                    {index + 1}
+                </div>
+            ))}
         </div>
     );
 }
